7f8976a5755812097150247535d21cb47e3cc2f3
daeb2a <daeb2a@gmail.com>
2023-01-02 16:51:04 +0900

    4.0 Introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-red-500">
-      <h1 className="text-black">it works</h1>
+      <h1 className="text-black text-center flex">it works</h1>
     </div>
   );
 }
