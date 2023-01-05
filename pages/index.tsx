c1325e8a9e7c125edae9cf41053084d0c50d661c
daeb2a <daeb2a@gmail.com>
2023-01-05 13:15:24 +0900

    5.1 Auth part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')] opacity-10">
-        <h2 className="text-[500px] text-[#000]">Hello</h2>
-      </div>
-    </>
-  );
+  return null;
 }
