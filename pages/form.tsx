64ba2b98c7dea1fdf44b3fa061da89714220ac7c
daeb2a <daeb2a@gmail.com>
2023-01-10 22:17:24 +0900

    7.5 Errors

--- pages/form.tsx
+++ pages/form.tsx
@@ -14,7 +14,9 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>({
+    mode: "onChange"
+  });
   const onValid = (data: LoginForm) => {
     console.log("im valid bby");
   };
@@ -43,10 +45,17 @@ export default function Forms() {
         placeholder="Username"
       />
       <input
-        {...register("email", { required: "Email is required" })}
+        {...register("email", { 
+          required: "Email is required", 
+          validate: {
+            notGmail: (value) => 
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+        } })}
         type="email"
         placeholder="Email"
+        className={`${Boolean(errors.email) ? "border-red-500" : ""}`}
       />
+      {errors.email?.message}
       <input
         {...register("password", { required: "Password is required" })}
         type="password"
