5b32e68481481fa8e9881da6aab30a4435b86d6f
daeb2a <daeb2a@gmail.com>
2023-01-14 00:10:37 +0900

    9.7 Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
@@ -7,8 +8,21 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
-  res.status(200).end()
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    /* include: { user: true } */
+  });
+  if(!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId
+  }
+  await req.session.save();
+  res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+    cookieName: "carrotsession",
+    password: "382901830918323121321321509832948102984091840938091284902801823190",
+});
