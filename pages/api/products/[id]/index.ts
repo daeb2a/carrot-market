53addfa9cf5e23daae2b1aa046ccb7c5d480c9a1
daeb2a <daeb2a@gmail.com>
2023-01-17 17:39:57 +0900

    11.7 Favorite Products part Two

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,7 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const { query: { id }, session: { user } } = req;
   const product = await client.product.findUnique({
     where: {
       id: Number(id),
@@ -37,8 +37,19 @@ async function handler(
         }
       }
     },
-  })
-  res.json({ ok: true, product, relatedProducts });
+  });
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user?.id,
+      },
+      select: {
+        id: true,
+      }
+    })
+  );
+  res.json({ ok: true, product, isLiked, relatedProducts });
 }
 
 export default withApiSession(
