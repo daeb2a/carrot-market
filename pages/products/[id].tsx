9301dfaee52ad63f9663eeeec8481ca445e232c6
daeb2a <daeb2a@gmail.com>
2023-01-17 18:23:20 +0900

    11.9 Unbound Mutations

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,11 +2,12 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import { useRouter } from "next/router";
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import Link from "next/link";
 import { Product, User } from "@prisma/client";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
+import useUser from "@libs/client/useUser";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -20,18 +21,20 @@ interface ItemDetailResponse {
 }
 
 const ItemDetail: NextPage = () => {
+  // const { user, isLoading } = useUser();
+  // const { mutate } = useSWRConfig();
   const router = useRouter();
-  console.log(router.query);
-  const { data, mutate } = useSWR<ItemDetailResponse>(
+  const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
-  console.log(data);
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavClick = () => {
     if (!data) return;
-    mutate({ ...data, isLiked: !data.isLiked }, false);
+    boundMutate((prev) => prev && { ...prev, isLiked: !prev.isLiked }, false);
     toggleFav({});
-  }
+    // mutate("/api/users/me", (prev: any) => ({ ok: !prev.ok }), false); // unbound mutation
+    // mutate("/api/users/me"); // simple refetch
+  } 
   return (
     <Layout canGoBack>
       <div className="px-4 py-4">
