de25f7e417e54e5616b90f1b5721268713df9ba6
daeb2a <daeb2a@gmail.com>
2023-01-18 12:54:25 +0900

    12.1 Forms and Handlers

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,13 +2,39 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+import { Post } from "@prisma/client";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [ post, { loading, data } ] = useMutation<WriteResponse>("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+    post(data);
+  }
+  useEffect(() => {
+    if(data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router])
   return (
-    <Layout canGoBack>
-      <form className="px-4 py-10">
-        <TextArea placeholder="Ask a Question!" required />
-        <Button text="Submit" />
+    <Layout title="Write Post" canGoBack>
+      <form onSubmit={handleSubmit(onValid)} className="px-4 py-10">
+        <TextArea register={register("question", { required: true, minLength: 5 })} required placeholder="Ask a Question!" />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
