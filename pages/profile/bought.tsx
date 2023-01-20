81ef22157bd1bae56fcca3887cb6ea07032a80e0
daeb2a <daeb2a@gmail.com>
2023-01-21 00:21:57 +0900

    13.4 Sales, Purchases, Favorites

--- pages/profile/bought.tsx
+++ pages/profile/bought.tsx
@@ -1,21 +1,12 @@
 import type { NextPage } from "next";
-import Item from "@components/item";
 import Layout from "@components/layout";
+import ProductList from "@components/product-list";
 
 const Bought: NextPage = () => {
   return (
     <Layout title="구매내역" canGoBack>
       <div className="flex flex-col space-y-5 py-10 divide-y">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            key={i}
-            id={i}
-            title="New IPhone 14"
-            price={1000000}
-            hearts={1}
-            comments={1}
-          />
-        ))}
+        <ProductList kind="purchases" />
       </div>
     </Layout>
   );
