16f13f635c904ab44a7e2ca17ee21a7cf0942e63
daeb2a <daeb2a@gmail.com>
2023-01-23 02:14:41 +0900

    14.2 Send Messages

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -4,23 +4,43 @@ import Message from "@components/message";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { Stream } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
 
 interface StreamResponse {
   ok: boolean;
   stream: Stream;
 }
+
+interface MessageForm {
+  message: string;
+}
+
 const Stream: NextPage = () => {
   const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data } = useSWR<StreamResponse>(
     router.query.id ? `/api/streams/${router.query.id}` : null
   );
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`
+  );
+  const onValid = (form: MessageForm) => {
+    if (loading) return;
+    reset();
+    sendMessage(form);
+  };
   return (
     <Layout canGoBack>
       <div className="py-10 px-4 space-y-4">
         <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
         <div className="mt-5">
-          <h1 className="text-3xl font-bold text-gray-900">{data?.stream?.name}</h1>
-          <span className="text-2xl block mt-3 text-gray-900">${data?.stream?.price}</span>
+          <h1 className="text-3xl font-bold text-gray-900">
+            {data?.stream?.name}
+          </h1>
+          <span className="text-2xl block mt-3 text-gray-900">
+            ${data?.stream?.price}
+          </span>
           <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
         </div>
         <div>
@@ -39,9 +59,13 @@ const Stream: NextPage = () => {
             <Message message="I want ￦20,000" reversed />
             <Message message="미쳤어" />
           </div>
-          <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
-            <div className="flex relative max-w-md items-center  w-full mx-auto">
+          <div className="fixed py-2 bg-white bottom-0 inset-x-0">
+            <form
+              onSubmit={handleSubmit(onValid)}
+              className="flex relative max-w-md items-center  w-full mx-auto"
+            >
               <input
+                {...register("message", { required: true })}
                 type="text"
                 className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
               />
@@ -50,7 +74,7 @@ const Stream: NextPage = () => {
                   &rarr;
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
