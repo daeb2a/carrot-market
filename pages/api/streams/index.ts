98f44e34ee20cabf4e45477a1ec6221904d82ccd
daeb2a <daeb2a@gmail.com>
2023-01-23 21:23:05 +0900

    14.6 Pagination

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -8,7 +8,10 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   if (req.method === "GET") {
-    const streams = await client.stream.findMany();
+    const streams = await client.stream.findMany({
+      take: 10,
+      skip: 20,
+    });
     res.json({
       ok: true,
       streams,
