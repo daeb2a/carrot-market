0dc7ed4c165976ab051732ad1560d437bd671de8
daeb2a <daeb2a@gmail.com>
2023-01-26 14:30:50 +0900

    17.2 Streaming API

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -13,7 +13,7 @@ async function handler(
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+        "Authorization": `Bearer ${process.env.CLOUDFLARE_IMAGE_TOKEN}`,
       },
     }
   )).json();
