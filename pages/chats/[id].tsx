7f1053cc572d3a8c804ba32961bdfc4edd016da4
daeb2a <daeb2a@gmail.com>
2023-01-28 23:36:43 +0900

    18.2 Create ChatMessages

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -1,87 +1,56 @@
+import Message from "@components/message";
+import useMutation from "@libs/client/useMutation";
+import { ChatRoom } from "@prisma/client";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import useSWR from "swr";
+
+interface ChatMessagesResponse {
+  ok: boolean;
+  chatRoom: ChatRoom[];
+}
+
+interface MessageForm {
+  message: string;
+}
 
 const ChatDetail: NextPage = () => {
+  const router = useRouter();
+  const {
+    data: messageData
+  } = useSWR<ChatMessagesResponse>(
+    router.query.id ? `/api/chats/${router.query.id}` : null
+  );
+  const { register, handleSubmit } = useForm<MessageForm>();
+  const [sendMessage, { data, loading }] = useMutation(`/api/chats/${router.query.id}/messages`);
+  const onValid = (form: MessageForm) => {
+    if (loading) return;
+    sendMessage(form);
+  };
   return (
     <div className="py-10 px-4 space-y-4">
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</p>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</p>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</p>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</p>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</p>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">Hi how much are you selling them for?</p>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">I want ￦20,000</p>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="w-8 h-8 rounded-full bg-slate-300" />
-        <p className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">미쳤어</p>
-      </div>
+      <>
+        {messageData?.chatRoom?.chatMessages.map((message) => (
+          <Message key={message.createdAt} message={message.message} />
+        ))}
+      </>
       <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
-        <div className="flex relative items-center">
-          <input type="text" className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:border-orange-500 pr-12" />
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex relative items-center"
+        >
+          <input
+            {...register("message", { minLength: 1 })}
+            type="text"
+            className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:border-orange-500 pr-12"
+          />
           <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-            <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600 cursor-pointer">&rarr;</button>
+            <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600 cursor-pointer">
+              &rarr;
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -3,6 +3,8 @@ import Layout from "@components/layout";
 import useSWR from 'swr';
 import { useRouter } from "next/router";
 import { ChatRoom } from "@prisma/client";
+import { useEffect } from "react";
+import Link from "next/link";
 
 interface ChatRoomsResponse {
   ok: boolean;
@@ -10,23 +12,25 @@ interface ChatRoomsResponse {
 }
 
 const Chats: NextPage = () => {
+  const router = useRouter();
   const { data } = useSWR<ChatRoomsResponse>(`/api/chats/`);
   return (
     <Layout hasTabBar title="채팅">
       <div className="divide-y-[1px] ">
         {data?.chatRooms.map((chatRoom) => (
-          <div
-            key={chatRoom.id}
-            className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-          >
-            <div className="w-12 h-12 rounded-full bg-slate-300" />
-            <div>
-              <p className="text-gray-700">{chatRoom.chatMessages[0]?.userId}</p>
-              <p className="text-sm  text-gray-500">
-                {chatRoom.chatMessages[0]?.message}
-              </p>
+          <Link legacyBehavior key={chatRoom.id} href={`/chats/${chatRoom.id}`}>
+            <div className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+              <div className="w-12 h-12 rounded-full bg-slate-300" />
+              <div>
+                <p className="text-gray-700">
+                  {chatRoom.chatMessages[0]?.userId}
+                </p>
+                <p className="text-sm  text-gray-500">
+                  {chatRoom.chatMessages[0]?.message}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </Layout>
