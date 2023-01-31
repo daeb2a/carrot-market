e8e517fd4960b2b9d728abef64c51c24a82d09a3
daeb2a <daeb2a@gmail.com>
2023-01-31 14:19:40 +0900

    19.1 Response and Redirections

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,22 @@
-import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextRequest, NextFetchEvent, userAgent } from "next/server";
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/((?!_next|api/auth).*)(.+)"],
+};
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log("middleware");
+  const ua = userAgent(req);
+  if (ua?.isBot) {
+    return new Response("Plz don't be a bot. Be human.", { status: 403 });
+  }
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.has("carrotsession")) {
+      return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
+  //  return NextResponse.json({ ok: true });
   if (req.nextUrl.pathname.startsWith("/chats")) {
-      console.log("chats ONLY middleware");
+    console.log("chats ONLY middleware");
   }
 }
